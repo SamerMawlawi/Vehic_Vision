107787d5950f8ff70a050b248da8290ee4dcac60
Azzam Abdullah <Azzam56464@gmail.com>
2026-03-03 19:57:30 +0300

    added master_mapper_IDs file (#10)
    * Update member name in README
    
    * I added master_mapper file
    
    * I added master_mapper_IDs file
    
    * I added master_mapper_IDs file
    
    * Forget the first one , this is the important one
    
    ---------
    
    Co-authored-by: SamerMawlawi <s445007760@uqu.edu.sa>

---
+++ src/utils/master_mapper_IDs.py
@@ -0,0 +1,34 @@
+master_mapping = {
+    #Damages
+    11380051: 0,  # Missing part
+    11380052: 1,  # Broken part
+    11380053: 2,  # Scratch
+    11380054: 3,  # Cracked
+    11380055: 4,  # Dent
+    11380056: 5,  # Flaking
+    11380057: 6,  # Paint chip
+    11380058: 7,  # Corrosion
+
+    # Parts
+    11380316: 8,   # Quarter-panel
+    11380317: 9,   # Front-wheel
+    11380318: 10,  # Back-window
+    11380319: 11,  # Trunk
+    11380320: 12,  # Front-door
+    11380321: 13,  # Rocker-panel
+    11380322: 14,  # Grille
+    11380323: 15,  # Windshield
+    11380324: 16,  # Front-window
+    11380325: 17,  # Back-door
+    11380326: 18,  # Headlight
+    11380327: 19,  # Back-wheel
+    11380328: 20,  # Back-windshield
+    11380329: 21,  # Hood
+    11380330: 22,  # Fender
+    11380331: 23,  # Tail-light
+    11380332: 24,  # License-plate
+    11380333: 25,  # Front-bumper
+    11380334: 26,  # Back-bumper
+    11380335: 27,  # Mirror
+    11380336: 28   # Roof
+}
